c99a4770d4f10456d4d190eabe4c806666a077e2
Sabyrka <sabyrkazen@gmail.com>
2026-02-15 12:56:48 +0500

    feat: implement occasion selection feature in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react'
+import { OccasionType } from './types'
+
 export const App = () => {
+  const [occasion, setOccasion] = useState<OccasionType>(OccasionType.BIRTHDAY)
+
   return (
-    <div>
-      App
+    <div className="min-h-screen bg-[#FAF5FF]">
+      <header></header>
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
+        <div className="max-w-7xl mx-auto">
+          {occasion}
+        </div>
+        <div>
+          <button onClick={() => setOccasion(OccasionType.BIRTHDAY)}>День Рождения</button>
+          <button onClick={() => setOccasion(OccasionType.NEW_YEAR)}>Новый Год</button>
+        </div>
+      </main>
     </div>
   )
 }
