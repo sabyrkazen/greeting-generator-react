ea14ca8ffa4aab253d71a7a152b0944defd18251
Sabyrka <sabyrkazen@gmail.com>
2026-02-15 13:02:41 +0500

    feat: add user input fields for name, age, and interests in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,38 @@ import { OccasionType } from './types'
 
 export const App = () => {
   const [occasion, setOccasion] = useState<OccasionType>(OccasionType.BIRTHDAY)
+  const [name, setName] = useState<string>('')
+  const [age, setAge] = useState<string>('')
+  const [interests, setInterests] = useState<string>('')
 
   return (
     <div className="min-h-screen bg-[#FAF5FF]">
       <header></header>
       <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
         <div className="max-w-7xl mx-auto">
-          {occasion}
+          <p>{occasion}</p>
+          <p>{name}</p>
+          <p>{age}</p>
+          <p>{interests}</p>
         </div>
         <div>
-          <button onClick={() => setOccasion(OccasionType.BIRTHDAY)}>День Рождения</button>
-          <button onClick={() => setOccasion(OccasionType.NEW_YEAR)}>Новый Год</button>
+          <div>
+            <button onClick={() => setOccasion(OccasionType.BIRTHDAY)}>День Рождения</button>
+            <button onClick={() => setOccasion(OccasionType.NEW_YEAR)}>Новый Год</button>
+          </div>
+          <div>
+            <input type="text" placeholder="Sabyrka" value={name} onChange={(event) => setName(event.target.value)} />
+            <br />
+            <input type="number" placeholder="18" value={age} onChange={(event) => setAge(event.target.value)} />
+            <br />
+            <textarea
+              cols={30}
+              rows={10}
+              placeholder='Например: "Любит футбол, путешествия и готовить"'
+              value={interests}
+              onChange={(event) => setInterests(event.target.value)}
+            ></textarea>
+          </div>
         </div>
       </main>
     </div>
